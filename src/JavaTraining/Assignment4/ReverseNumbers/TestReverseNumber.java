0b7f8bd3bbdeb0ae8745a8528df584cbebc9bea4
Fang Liu <fang.liu@acc.co.nz>
2021-04-08 15:59:03 +1200

    add assignment 1, reverse number

---
+++ src/JavaTraining/Assignment4/ReverseNumbers/TestReverseNumber.java
@@ -0,0 +1,26 @@
+package JavaTraining.Assignment4.ReverseNumbers;
+
+import java.util.Scanner;
+
+public class TestReverseNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Please inputInt an integer number: ");
+        int inputInt = scanner.nextInt();
+        String reverseInt = reverse(inputInt);
+        System.out.println("The reverseInt number of your inputInt is: " + reverseInt);
+
+    }
+
+    public static String reverse(int inputInt) {
+        String s = String.valueOf(inputInt);
+        int len = s.length();
+        StringBuilder dest = new StringBuilder(len);
+
+        for (int i = (len - 1); i >= 0; i--){
+            dest.append(s.charAt(i));
+        }
+        return dest.toString();
+    }
+}
