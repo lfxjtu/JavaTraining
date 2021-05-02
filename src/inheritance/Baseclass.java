7c152514b3dfa6cebbe8850dc160ef03709dbfad
Fang Liu <fang.liu@acc.co.nz>
2021-05-03 09:51:14 +1200

    delete unused ones

--- src/inheritance/Baseclass.java
+++
@@ -1,9 +0,0 @@
-package inheritance;
-
-public class Baseclass {
-    private String privateVar;
-    protected String protectedVar; //class + children + packages;
-    String variable; //Class + children in same package
-    public String publicVar; //all package
-
-}
